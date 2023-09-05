7c4d94963719fbef2e83266453204cb6bf2fb664
Abhishek Mali <abhishekrockstar545@gmail.com>
2023-09-05 18:26:02 +0530

    Task 1: Add validation constraints

--- src/main/java/com/innoventes/test/app/dto/CompanyDTO.java
+++ src/main/java/com/innoventes/test/app/dto/CompanyDTO.java
@@ -1,5 +1,13 @@
 package com.innoventes.test.app.dto;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PositiveOrZero;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,11 +21,19 @@ public class CompanyDTO {
 
 	private Long id;
 
+	@NotNull(message = "Company name is mandatory")
+	@NotEmpty(message = "Company name is mandatory")
+	@Size(min = 5, message = "Company name must be at least 5 characters long")
 	private String companyName;
 
+	@NotNull(message = "Email is mandatory")
+	@NotEmpty(message = "Email is mandatory")
+	@Email(message = "Email should be a valid email address")
 	private String email;
 
+	@PositiveOrZero(message = "Strength should be a positive number or zero")
 	private Integer strength;
 	
+	@URL(message = "Website URL should be a valid URL")
 	private String webSiteURL;
 }
